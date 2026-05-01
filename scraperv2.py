1d11803c4b0d4fe5a94f9f6bbd7e6e3119ca884d
frankngugi <frankngugi@gmail.com>
2026-05-01 21:45:29 +0530

    added some code with beautiful soup

--- scraperv2.py
+++ scraperv2.py
@@ -2,3 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
+
+url = 'https://www.nehruplacemarket.com/price-list/ram-price-list.html?pagenum=1'
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content,"html.parser")
+
+print(soup.title.text)
